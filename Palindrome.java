e7e7cf8bcacf159b96d0a92b2d10af5625cb80c3
rimelam <>
2021-04-08 19:24:09 +0000

    je propose cette solution avec une boucle while

--- Palindrome.java
+++ Palindrome.java
@@ -1,7 +1,18 @@
 public class Palindrome{
     public static boolean is_palindrome(String s)
-    {
-        // add your code here
-        return true;
+    { 
+    int i=0;
+    boolean pal=true;
+    int longueur=s.length()-1;
+    while(i<(longueur/2) && pal){
+      if(s.charAt(i)==s.charAt(longueur)){
+        i++;
+        pal=true;
+      }
+      else{
+        pal=false;
+      }
     }
+    return pal;
+}
 }
--- Principal.java
+++ Principal.java
@@ -5,7 +5,7 @@ public class Principal
 	
 		if ( args.length == 1 )
 		{
-			String motAverifier = args[0] ; // args[0] correspond a l'argument 2 de la commande java
+			String motAverifier = args[0] ; 
 		
 			boolean isPalindrome = Palindrome.is_palindrome(motAverifier) ;
 		
